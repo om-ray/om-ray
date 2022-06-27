59756936c8dce322118a9a95d9c41b35064c932f
123om123 <omihridesh@gmail.com>
2022-06-27 11:01:25 +0530

    testing onload function

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-let skillObjArr = [];
-let skills = document.querySelectorAll(".tag");
+let init = function () {
+  let skillObjArr = [];
+  let skills = document.querySelectorAll(".tag");
 
-skills.forEach((skill) => {
-  skillObjArr.push({
-    skillParent: skill,
-    skillName: skill.id,
-    skillImage:
-      skill.id == "web3js"
-        ? "./Images/web3js.jpeg"
-        : `https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/${skill.id}.svg`,
-    transitionImage: `./Images/${skill.id}-transitionColor.png`,
+  skills.forEach((skill) => {
+    skillObjArr.push({
+      skillParent: skill,
+      skillName: skill.id,
+      skillImage:
+        skill.id == "web3js"
+          ? "./Images/web3js.jpeg"
+          : `https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/${skill.id}.svg`,
+      transitionImage: `./Images/${skill.id}-transitionColor.png`,
+    });
   });
-});
 
-skillObjArr.map((skillObj) => {
-  new hoverEffect({
-    parent: skillObj.skillParent,
-    image1: skillObj.skillImage,
-    image2: skillObj.transitionImage,
-    displacementImage: "./Images/displacementImage.png",
+  skillObjArr.map((skillObj) => {
+    new hoverEffect({
+      parent: skillObj.skillParent,
+      image1: skillObj.skillImage,
+      image2: skillObj.transitionImage,
+      displacementImage: "./Images/displacementImage.png",
+    });
   });
-});
+};
+
+document.addEventListener("load", init);
